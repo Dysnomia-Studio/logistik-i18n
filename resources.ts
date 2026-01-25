2fe2964f867ea47c5deca622b8360cd5b2cd2267
Elanis <elanis@hotmail.com>
2026-01-25 11:52:21 +0100

    More translations

--- resources.ts
+++ resources.ts
@@ -1,9 +1,11 @@
 // This file is needed because vite doesn't like dynamic import when exporting ...
 import achievements_en from './achievements/en.json';
+import credits_en from './credits/en.json';
 import menu_en from './menu/en.json';
 import toolbelt_en from './toolbelt/en.json';
 
 import achievements_fr from './achievements/fr.json';
+import credits_fr from './credits/fr.json';
 import menu_fr from './menu/fr.json';
 import toolbelt_fr from './toolbelt/fr.json';
 
@@ -12,12 +14,14 @@ import config from './config.json';
 const resources: { [key: string]: { [key: string]: { [key: string]: string } } } = {};
 resources['en'] = {
 	achievements: achievements_en,
+	credits: credits_en,
 	languages: config.languages,
 	menu: menu_en,
 	toolbelt: toolbelt_en,
 };
 resources['fr'] = {
 	achievements: achievements_fr,
+	credits: credits_fr,
 	languages: config.languages,
 	menu: menu_fr,
 	toolbelt: toolbelt_fr,
